738c53d5022be2fbb9617ad48aeec15f7755eb40
Gustavo Marino <gamarino@numaes.com>
2026-02-04 00:02:29 -0300

    chore(protoPython): exec engine tests v8

--- test/library/TestExecutionEngine.cpp
+++ test/library/TestExecutionEngine.cpp
@@ -141,3 +141,68 @@ TEST(ExecutionEngineTest, CallFunction) {
     ASSERT_TRUE(result->isInteger(&ctx));
     EXPECT_EQ(result->asLong(&ctx), 42);
 }
+
+TEST(ExecutionEngineTest, LoadAttr) {
+    proto::ProtoSpace space;
+    proto::ProtoContext ctx(&space);
+    const proto::ProtoObject* obj = ctx.newObject(true);
+    proto::ProtoObject* mutableObj = const_cast<proto::ProtoObject*>(obj);
+    mutableObj->setAttribute(&ctx, proto::ProtoString::fromUTF8String(&ctx, "x"), ctx.fromInteger(42));
+    const proto::ProtoList* constants = ctx.newList()->appendLast(&ctx, obj);
+    const proto::ProtoList* names = ctx.newList()->appendLast(&ctx, ctx.fromUTF8String("x"));
+    const proto::ProtoList* bytecode = ctx.newList()
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_ATTR))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_RETURN_VALUE));
+    const proto::ProtoObject* result = protoPython::executeMinimalBytecode(
+        &ctx, constants, bytecode, names, nullptr);
+    ASSERT_NE(result, nullptr);
+    ASSERT_TRUE(result->isInteger(&ctx));
+    EXPECT_EQ(result->asLong(&ctx), 42);
+}
+
+TEST(ExecutionEngineTest, StoreAttr) {
+    proto::ProtoSpace space;
+    proto::ProtoContext ctx(&space);
+    const proto::ProtoObject* obj = ctx.newObject(true);
+    const proto::ProtoList* constants = ctx.newList()
+        ->appendLast(&ctx, obj)
+        ->appendLast(&ctx, ctx.fromInteger(99));
+    const proto::ProtoList* names = ctx.newList()->appendLast(&ctx, ctx.fromUTF8String("x"));
+    const proto::ProtoList* bytecode = ctx.newList()
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(1))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_STORE_ATTR))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_ATTR))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_RETURN_VALUE));
+    const proto::ProtoObject* result = protoPython::executeMinimalBytecode(
+        &ctx, constants, bytecode, names, nullptr);
+    ASSERT_NE(result, nullptr);
+    ASSERT_TRUE(result->isInteger(&ctx));
+    EXPECT_EQ(result->asLong(&ctx), 99);
+}
+
+TEST(ExecutionEngineTest, BuildList) {
+    proto::ProtoSpace space;
+    proto::ProtoContext ctx(&space);
+    const proto::ProtoList* constants = ctx.newList()
+        ->appendLast(&ctx, ctx.fromInteger(1))
+        ->appendLast(&ctx, ctx.fromInteger(2))
+        ->appendLast(&ctx, ctx.fromInteger(3));
+    const proto::ProtoList* bytecode = ctx.newList()
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(0))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(1))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_LOAD_CONST))->appendLast(&ctx, ctx.fromInteger(2))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_BUILD_LIST))->appendLast(&ctx, ctx.fromInteger(3))
+        ->appendLast(&ctx, ctx.fromInteger(protoPython::OP_RETURN_VALUE));
+    const proto::ProtoObject* result = protoPython::executeMinimalBytecode(
+        &ctx, constants, bytecode, nullptr, nullptr);
+    ASSERT_NE(result, nullptr);
+    const proto::ProtoList* list = result->asList(&ctx);
+    ASSERT_NE(list, nullptr);
+    EXPECT_EQ(list->getSize(&ctx), 3u);
+    EXPECT_EQ(list->getAt(&ctx, 0)->asLong(&ctx), 1);
+    EXPECT_EQ(list->getAt(&ctx, 1)->asLong(&ctx), 2);
+    EXPECT_EQ(list->getAt(&ctx, 2)->asLong(&ctx), 3);
+}
