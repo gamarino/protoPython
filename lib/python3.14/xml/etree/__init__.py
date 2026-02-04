09ae467aaadfcd9b9ae68bf54937480c45dabf6c
Gustavo Marino <gamarino@numaes.com>
2026-02-04 08:35:58 -0300

    stub(xml): add xml.etree or xml.dom stub

---
+++ lib/python3.14/xml/etree/__init__.py
@@ -0,0 +1,5 @@
+# xml.etree - minimal stub.
+
+def Element(tag, **kwargs):
+    """Stub: raises NotImplementedError."""
+    raise NotImplementedError("xml.etree.ElementTree.Element is not implemented")
