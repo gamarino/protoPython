80a8f3d93ab3db67b08afb678caaaf37a3e686f8
Gustavo Marino <gamarino@numaes.com>
2026-02-04 08:35:37 -0300

    stub(csv): add csv module stub

---
+++ lib/python3.14/csv.py
@@ -0,0 +1,9 @@
+# csv.py - Stub: reader, writer, or module attr.
+
+def reader(iterable, **kwargs):
+    """Stub: raises NotImplementedError."""
+    raise NotImplementedError("csv.reader is not implemented")
+
+def writer(fileobj, **kwargs):
+    """Stub: raises NotImplementedError."""
+    raise NotImplementedError("csv.writer is not implemented")
