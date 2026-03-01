d58eec5ee933c03fd9f52691aabc262222c1d7c5
gamarino <gamarino@gmail.com>
2026-03-01 03:06:12 -0300

    Add debugging scripts for GDB

---
+++ run_gdb_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PROTO_PYTHONPATH=/mnt/c/Users/gamar/PycharmProjects/protoPython/lib/python3.14
+export PROTO_PYTHONPATH
+gdb -batch -ex "run" -ex "thread apply all bt full" -ex "thread 2" -ex "frame 0" -ex "p *space" -ex "quit" --args /mnt/c/Users/gamar/PycharmProjects/protoPython/wsl-build/src/runtime/protopy lib/python3.14/test/test_grammar.py > gdb_out_all.txt 2>&1 &
+GDB_PID=$!
+sleep 15
+kill -INT $GDB_PID
+wait $GDB_PID
